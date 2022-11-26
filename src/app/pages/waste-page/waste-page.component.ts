d5b8d77121bd934d3ca1647028c1029eababd72a
Moemen <moemenhussein44@gmail.com>
2022-11-26 20:43:09 +0100

    FEAT: can get article by id

--- src/app/pages/waste-page/waste-page.component.ts
+++ src/app/pages/waste-page/waste-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpService} from "../../services/http.service";
 import {Article} from "../../models/article.model";
+import {ArticleDetailModel} from "../../models/article-detail.model";
 
 @Component({
   selector: 'app-waste-page',
@@ -11,16 +12,25 @@ export class WastePageComponent implements OnInit{
 
   ngOnInit() {
     this.getArticles();
+    this.getArticle();
   }
 
   constructor(private httpService: HttpService) {}
   articles: Article[] = []
+  article: Article | undefined
 
 
   public getArticles(): void{
     this.httpService.getData<Article>("api/article")
       .subscribe((_aricles)=>{
         this.articles = _aricles
+      })
+  }
+
+  getArticle(): void{
+    this.httpService.getSingleData<Article>("api/article/1")
+      .subscribe((_aricle)=>{
+        this.article = _aricle
       })
   }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {resetParseTemplateAsSourceFileForTest} from "@angular/compiler-cli/src/ngtsc/typecheck/diagnostics";
 import {Article} from "../models/article.model";
 import {Observable} from "rxjs";
@@ -9,9 +9,18 @@ export class HttpService{
   public url = "http://localhost:8080/"
   constructor(private http: HttpClient) {}
 
+  headers= new HttpHeaders()
+    .set('content-type', 'application/json')
+    .set('Access-Control-Allow-Origin', '*');
+
   public getData<T>(path: String): Observable<Array<T>>{
     return this.http
-      .get<T[]>(this.url+path)
+      .get<T[]>(this.url+path, {'headers':this.headers})
+  }
+
+  public getSingleData<T>(path: String): Observable<T>{
+    return this.http
+      .get<T>(this.url+path, {'headers':this.headers})
   }
 }
 
