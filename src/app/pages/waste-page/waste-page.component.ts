9b20b3331ece5c90b1b1a92fa6e374e6200b00cb
Moemen <moemenhussein44@gmail.com>
2022-11-26 01:55:44 +0100

    BUG: has to run get call 2 times to get data

--- src/app/pages/waste-page/waste-page.component.ts
+++ src/app/pages/waste-page/waste-page.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpService} from "../../services/http.service";
+import {Article} from "../../models/article.model";
 
 @Component({
   selector: 'app-waste-page',
   templateUrl: './waste-page.component.html',
   styleUrls: ['./waste-page.component.scss']
 })
-export class WastePageComponent {
+export class WastePageComponent implements OnInit{
+
+  ngOnInit() {
+    this.getApi();
+  }
+
   constructor(private httpService: HttpService) {}
+  articles: Article[] = [
+    new Article()
+  ]
+  newArticles: Object[] = []
+
 
   public getApi(){
-    this.httpService.getData("api/article")
+    this.httpService.getData("api/article", this.articles)
+    this.newArticles = this.httpService.data
+    // console.log(this.newArticles)
   }
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,16 +1,19 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {resetParseTemplateAsSourceFileForTest} from "@angular/compiler-cli/src/ngtsc/typecheck/diagnostics";
 
 @Injectable({providedIn: 'root'})
 export class HttpService{
-  private url = "http://localhost:8080/"
+  public url = "http://localhost:8080/"
   constructor(private http: HttpClient) {}
+  data: Object[] = [];
 
-  getData(path: String){
+  public getData(path: String, Model: Object){
     this.http
-      .get(this.url+path)
+      .get<typeof Model[]>(this.url+path)
       .subscribe(response => {
-        console.log(response)
+        this.data = response
+        console.log(this.data)
       })
   }
 }
