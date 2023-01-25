2b7a2a6144ab4aaac76a49aa8cefdb4e72f02944
Mohammed <s1136968@student.hsleiden.nl>
2023-01-25 13:18:14 +0100

    feat: orders now get removed from list and updated in database

--- src/app/pages/order/order-list/single-order-info/single-order-info/single-order-info.component.ts
+++ src/app/pages/order/order-list/single-order-info/single-order-info/single-order-info.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import {ArticleOrderModel} from "../../../../../models/ArticleOrder.model";import {ArticleLocationModel} from "../../../../../models/Article.Location.model";
 import {Article} from "../../../../../models/Waste/article.model";
+import {HttpService} from "../../../../../services/http.service";
 
 @Component({
   selector: 'app-single-order-info',
@@ -12,11 +13,16 @@ export class SingleOrderInfoComponent implements OnInit{
   ngOnInit() {
   }
 
+
+  constructor(private httpService: HttpService) {}
+
   @Input() wasteOrder: ArticleOrderModel
   @Input() numInList: number;
   @Output() newItemEvent = new EventEmitter<{ inpNum: number, article: ArticleOrderModel }>;
 
   public removeOrder(num: number, article: ArticleOrderModel){
-    this.newItemEvent.emit({inpNum: num, article: article})
+    this.httpService.updateData<ArticleOrderModel>("/api/v2/article_order/" + article.id + "/update", article).subscribe((article) => {
+      this.newItemEvent.emit({inpNum: num, article: article})
+    })
   }
 }
